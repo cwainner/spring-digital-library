c663746a93f6ec449ba6631d2de3140bfe62763a
Chris Wainner <chris.jwainner@gmail.com>
2018-04-04 17:58:16 -0700

    Add RootController index routing

--- src/main/java/controllers/RootController.java
+++ src/main/java/controllers/RootController.java
@@ -1,4 +1,13 @@
 package controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RootController {
+
+  @GetMapping("/")
+  public String index(){
+    return "index";
+  }
 }
